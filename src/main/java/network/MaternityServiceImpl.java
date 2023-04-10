249600c7d8024d60e7c161d4646324edfc23ccfc
Justas Galminas <j.galminas@gmail.com>
2023-04-10 12:00:33 +0100

    Implemented MaternityService

--- src/main/java/network/MaternityServiceImpl.java
+++ src/main/java/network/MaternityServiceImpl.java
@@ -4,6 +4,10 @@ import models.Admission;
 import models.Allocation;
 import models.Employee;
 import models.Patient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -18,22 +22,98 @@ public class MaternityServiceImpl implements MaternityService {
 
     @Override
     public CompletableFuture<List<Allocation>> getAllocations() {
-        return CompletableFuture.supplyAsync(Collections::emptyList);
+
+        CompletableFuture<List<Allocation>> future = new CompletableFuture<>();
+
+        maternityAPI.getAllocations().enqueue(new Callback<List<Allocation>>() {
+            @Override
+            public void onResponse(Call<List<Allocation>> call, Response<List<Allocation>> response) {
+                if (response.isSuccessful()) {
+                    future.complete(response.body());
+                } else {
+                    future.complete(Collections.emptyList());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Allocation>> call, Throwable t) {
+                future.completeExceptionally(t);
+            }
+        });
+
+        return future;
     }
 
     @Override
     public CompletableFuture<List<Admission>> getAdmissions() {
-        return null;
+
+        CompletableFuture<List<Admission>> future = new CompletableFuture<>();
+
+        maternityAPI.getAdmissions().enqueue(new Callback<List<Admission>>() {
+            @Override
+            public void onResponse(Call<List<Admission>> call, Response<List<Admission>> response) {
+                if (response.isSuccessful()) {
+                    future.complete(response.body());
+                } else {
+                    future.complete(Collections.emptyList());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Admission>> call, Throwable t) {
+                future.completeExceptionally(t);
+            }
+        });
+
+        return future;
     }
 
     @Override
     public CompletableFuture<List<Employee>> getEmployees() {
-        return null;
+
+        CompletableFuture<List<Employee>> future = new CompletableFuture<>();
+
+        maternityAPI.getEmployees().enqueue(new Callback<List<Employee>>() {
+            @Override
+            public void onResponse(Call<List<Employee>> call, Response<List<Employee>> response) {
+                if (response.isSuccessful()) {
+                    future.complete(response.body());
+                } else {
+                    future.complete(Collections.emptyList());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Employee>> call, Throwable t) {
+                future.completeExceptionally(t);
+            }
+        });
+
+        return future;
     }
 
     @Override
     public CompletableFuture<List<Patient>> getPatients() {
-        return null;
+
+        CompletableFuture<List<Patient>> future = new CompletableFuture<>();
+
+        maternityAPI.getPatients().enqueue(new Callback<List<Patient>>() {
+            @Override
+            public void onResponse(Call<List<Patient>> call, Response<List<Patient>> response) {
+                if (response.isSuccessful()) {
+                    future.complete(response.body());
+                } else {
+                    future.complete(Collections.emptyList());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Patient>> call, Throwable t) {
+                future.completeExceptionally(t);
+            }
+        });
+
+        return future;
     }
 
 }
