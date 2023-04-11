1747831517ab8aae6e5c70933fd82d082116947f
Justas Galminas <j.galminas@gmail.com>
2023-04-11 16:41:38 +0100

    Created retrofit client tests

---
+++ src/test/java/network/RetrofitClientTest.java
@@ -0,0 +1,25 @@
+package network;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RetrofitClientTest {
+
+    RetrofitClient retrofitClient = RetrofitClient.getInstance();
+
+    @Test
+    void testRetrofitClientNotNull() {
+        assertNotNull(retrofitClient);
+    }
+
+    @Test
+    void testGetMaternityAPI() {
+        assertInstanceOf(MaternityAPI.class, retrofitClient.getMaternityAPI());
+    }
+
+    @Test
+    void testMaternityAPINotNull() {
+        assertNotNull(retrofitClient.getMaternityAPI());
+    }
+}
