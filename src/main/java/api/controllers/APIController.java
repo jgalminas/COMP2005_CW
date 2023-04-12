d0b5f2df292d2a7d6ad06232be33483b0c586d8e
Justas Galminas <j.galminas@gmail.com>
2023-04-12 15:58:22 +0100

    Tested/implemented api endpoints

---
+++ src/main/java/api/controllers/APIController.java
@@ -0,0 +1,47 @@
+package api.controllers;
+
+import api.services.APIService;
+import api.models.AdmissionDuration;
+import api.models.Day;
+import api.models.Patient;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class APIController {
+
+    private final APIService apiService;
+
+    @Autowired
+    public APIController(APIService apiService) {
+        this.apiService = apiService;
+    }
+
+    @Operation(summary = "Get the day of the week which has the most amount of admissions.")
+    @GetMapping(value = "day-with-most-admissions", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Day getDayWithMostAdmissions() {
+        return apiService.getDayWithMostAdmissions();
+    }
+
+    @Operation(summary = "Get the average patient admission duration for a specific employee.",
+            description = "If an admission has an invalid date format, the duration will be set to 0, therefore, affecting the result.")
+    @GetMapping(value = "avg-patient-time/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public AdmissionDuration getAvgPatientTimeByEmployeeId(@PathVariable("id") int id) {
+        return apiService.getAvgPatientTimeByEmployeeId(id);
+    }
+
+    @Operation(summary = "Get a list of patients seen by a specific employee.")
+    @GetMapping(value = "patients-by-employee/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Patient> getPatientsByEmployeeId(@PathVariable("id") int id) {
+        return apiService.getPatientsByEmployeeId(id);
+    }
+
+    @Operation(summary = "Get a list of patients which were discharged within 3 days of admission.")
+    @GetMapping(value = "discharged-within-3-days", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Patient> getPatientsDischargedWithin3Days() {
+        return apiService.getPatientsDischargedWithin3Days();
+    }
+}
---
+++ src/test/java/api/APIControllerTest.java
@@ -0,0 +1,141 @@
+package api;
+
+import api.models.AdmissionDuration;
+import api.models.Day;
+import api.models.Patient;
+import api.services.APIService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureMockMvc
+@WebMvcTest
+class APIControllerTest {
+
+    @MockBean
+    APIService apiService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void testGetDayWithMostAdmissions() throws Exception {
+
+        when(apiService.getDayWithMostAdmissions()).thenReturn(new Day("MONDAY"));
+
+        String expected = "{\"name\":\"MONDAY\"}";
+        String result = mockMvc.perform(get("/day-with-most-admissions"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertEquals(expected, result);
+
+    }
+
+    @Test
+    void testGetAvgPatientTimeByEmployeeId() throws Exception {
+
+        int id = 4;
+
+        when(apiService.getAvgPatientTimeByEmployeeId(id)).thenReturn(new AdmissionDuration(2934));
+
+        String expected = "{\"unit\":\"MINUTES\",\"amount\":2934}";
+        String result = mockMvc.perform(get("/avg-patient-time/{id}", id))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertEquals(expected, result);
+
+    }
+
+    @Test
+    void testGetPatientsByEmployeeId() throws Exception {
+
+        int id = 4;
+
+        when(apiService.getPatientsByEmployeeId(id)).thenReturn(Arrays.asList(
+                new Patient(1, "Robinson", "Viv", "1113335555"),
+                new Patient(2, "Carter", "Heather", "2224446666")
+        ));
+
+        String expected = "[{\"id\":1,\"surname\":\"Robinson\",\"forename\":\"Viv\",\"nhsNumber\":\"1113335555\"}," +
+                          "{\"id\":2,\"surname\":\"Carter\",\"forename\":\"Heather\",\"nhsNumber\":\"2224446666\"}]";
+
+        String result = mockMvc.perform(get("/patients-by-employee/{id}", id))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertEquals(expected, result);
+
+    }
+
+    @Test
+    void testGetPatientsByEmployeeId_EmptyArray() throws Exception {
+
+        int id = 4;
+
+        when(apiService.getPatientsByEmployeeId(id)).thenReturn(Collections.emptyList());
+
+        String expected = "[]";
+
+        String result = mockMvc.perform(get("/patients-by-employee/{id}", id))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertEquals(expected, result);
+
+    }
+
+    @Test
+    void testGetPatientsDischargedWithin3Days_EmptyArray() throws Exception {
+
+        when(apiService.getPatientsDischargedWithin3Days()).thenReturn(Collections.emptyList());
+
+        String expected = "[]";
+        String result = mockMvc.perform(get("/discharged-within-3-days"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertEquals(expected, result);
+
+    }
+
+    @Test
+    void testGetPatientsDischargedWithin3Days() throws Exception {
+
+        when(apiService.getPatientsDischargedWithin3Days()).thenReturn(Arrays.asList(
+                new Patient(2, "Carter", "Heather", "2224446666")
+        ));
+
+        String expected = "[{\"id\":2,\"surname\":\"Carter\",\"forename\":\"Heather\",\"nhsNumber\":\"2224446666\"}]";
+        String result = mockMvc.perform(get("/discharged-within-3-days"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertEquals(expected, result);
+
+    }
+}
